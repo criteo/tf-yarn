83171437ce82c6e7e26f6e124372127bcca4989d
Sergei Lebedev <s.lebedev@criteo.com>
2018-09-21 16:43:55 +0200

    Fixed Linux version check in ``reserve_sock_addr``

--- tf_yarn/_internal.py
+++ tf_yarn/_internal.py
@@ -58,23 +58,30 @@ class MonitoredThread(Thread):
             self._exc = exc
 
 
+def get_so_reuseport():
+    try:
+        return socket.SO_REUSEPORT
+    except AttributeError:
+        if platform.system() == "Linux":
+            major, minor, *_ = platform.release().split(".")
+            if (int(major), int(minor)) > (3, 9):
+                # The interpreter must have been compiled on Linux <3.9.
+                return 15
+    return None
+
+
 @contextmanager
 def reserve_sock_addr() -> typing.Iterator[typing.Tuple[str, int]]:
     """Reserve an available TCP port to listen on.
 
-    The acquired TCP socket is created with ``SO_REUSEPORT`` flag set
-    and is kept open until the generator is closed.
+    The reservation is done by binding a TCP socket to port 0 with
+    ``SO_REUSEPORT`` flag set (requires Linux >=3.9). The socket is
+    then kept open until the generator is closed.
     """
-    try:
-        so_reuseport = socket.SO_REUSEPORT
-    except AttributeError:
-        if platform.system() == "Linux" and platform.release() >= "3.9":
-            # The interpreter must have been compiled on Linux <3.9.
-            so_reuseport = 15
-        else:
-            raise RuntimeError(
-                "SO_REUSEPORT is not supported by the operating system"
-            ) from None
+    so_reuseport = get_so_reuseport()
+    if so_reuseport is None:
+        raise RuntimeError(
+            "SO_REUSEPORT is not supported by the operating system") from None
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
         sock.setsockopt(socket.SOL_SOCKET, so_reuseport, 1)

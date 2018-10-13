a98dda16bac12507aa9a2534a36e13719597915f
n.fraison <n.fraison@criteo.com>
2018-10-13 21:56:03 +0200

    Remove useless override of LD_LIBRARY_PATH
    Change-Id: I4a4e5b57828356314eacb1c91b068292e2726c2e

--- tf_yarn/_criteo.py
+++ tf_yarn/_criteo.py
@@ -10,18 +10,3 @@ def get_requirements_file():
         return "criteo.requirements.txt"
     else:
         return "requirements.txt"
-
-
-def get_default_env():
-    if not is_criteo():
-        return {}
-
-    # The following is specific to the Criteo infra. Moreover, the
-    # definitions assume that the system submitting the application
-    # is "sufficiently close" to that of the containers.
-    return {
-        "LD_LIBRARY_PATH": ":".join([
-            f"{os.environ['JAVA_HOME']}/jre/lib/amd64/server",
-            "/usr/lib/hadoop-criteo/hadoop/lib/native"
-        ])
-    }

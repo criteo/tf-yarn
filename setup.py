1a654730966172d2cdafeeeda5e3a727e62fc806
Gaëtan RACIC <g.racic@criteo.com>
2018-12-18 12:55:49 +0000

    Increment version of TF-YARN
    Change-Id: I033d10e15a9082e3cd1b6d0c272e881b83749f1a

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ setup(
     packages=["tf_yarn"],
     include_package_data=True,
     package_data={"tf_yarn": ["default.log.conf"]},
-    version=_check_add_criteo_environment("0.1.8"),
+    version=_check_add_criteo_environment("0.1.9"),
     install_requires=[
         "tensorflow==1.12.0",
         "dill==0.2.8",

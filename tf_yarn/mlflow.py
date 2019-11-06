f40cdf4da57097a193cd655d24703a513e5975c5
Fabian Höring <f.horing@criteo.com>
2019-11-06 14:12:15 +0100

    Factorize safe_mlflow/optional_mlflow wrappers
    Change-Id: I8c5f345c97a79c6d6053d6f5001b18f8c40c0767

--- tf_yarn/mlflow.py
+++ tf_yarn/mlflow.py
@@ -54,82 +54,71 @@ def _is_pyarrow_installed():
         return False
 
 
-def safe_mlflow(f):
-    @wraps(f)
-    def wrapper(*args, **kwds):
-        try:
-            return f(*args, **kwds)
-        except (ConnectionError, requests_exc.RequestException, exceptions.MlflowException):
-            logger.exception("mlflow connection error")
-    return wrapper
+def optional_mlflow(return_default=None):
+    def optional_mlflow_decorator(f):
+        @wraps(f)
+        def wrapper(*args, **kwds):
+            if use_mlflow():
+                try:
+                    return f(*args, **kwds)
+                except (ConnectionError, requests_exc.RequestException, exceptions.MlflowException):
+                    logger.exception("mlflow connection error")
+            else:
+                return return_default
+        return wrapper
+    return optional_mlflow_decorator
 
 
-def optional_mlflow(f):
-    @wraps(f)
-    def wrapper(*args, **kwds):
-        if use_mlflow():
-            try:
-                return f(*args, **kwds)
-            except (ConnectionError, requests_exc.RequestException, exceptions.MlflowException):
-                logger.exception("mlflow connection error")
-    return wrapper
-
-
-@safe_mlflow
+@optional_mlflow(return_default="")
 def active_run_id() -> str:
-    if use_mlflow():
-        active_run = mlflow.active_run()
-        if not active_run:
-            logger.warning("there is no active mlflow run. starting a new run ..")
-            active_run = mlflow.start_run()
-        return active_run.info.run_id
-    else:
-        return ""
+    active_run = mlflow.active_run()
+    if not active_run:
+        logger.warning("there is no active mlflow run. starting a new run ..")
+        active_run = mlflow.start_run()
+    return active_run.info.run_id
 
 
+@optional_mlflow(return_default="")
 def get_tracking_uri() -> str:
-    if use_mlflow():
-        return mlflow.get_tracking_uri()
-    else:
-        return ""
+    return mlflow.get_tracking_uri()
 
 
-@optional_mlflow
+@optional_mlflow()
 def set_tag(key: str, value: Any):
     mlflow.set_tag(key, value)
 
 
-@optional_mlflow
+@optional_mlflow()
 def set_tags(tags: Dict[str, Any]):
     mlflow.set_tags(tags)
 
 
-@optional_mlflow
+@optional_mlflow()
 def log_param(key: str, value: Any):
     mlflow.log_param(key, value)
 
 
-@optional_mlflow
+@optional_mlflow()
 def log_params(params: Dict[str, Any]):
     mlflow.log_params(params)
 
 
-@optional_mlflow
+@optional_mlflow()
 def log_metric(key: str, value: float, step: int = None):
     mlflow.log_metric(key, value, step)
 
 
-@optional_mlflow
+@optional_mlflow()
 def log_metrics(metrics: Dict[str, Any], step: int = None):
     mlflow.log_metrics(metrics, step)
 
 
-@optional_mlflow
+@optional_mlflow()
 def log_artifact(local_path: str, artifact_path: str = None):
     mlflow.log_artifact(local_path, artifact_path)
 
 
-@optional_mlflow
+@optional_mlflow()
 def log_artifacts(local_dir: str, artifact_path: str = None):
     mlflow.log_artifacts(local_dir, artifact_path)
 
@@ -141,11 +130,8 @@ def format_key(key: str) -> str:
         return ""
 
 
-@safe_mlflow
+@optional_mlflow()
 def save_text_to_mlflow(content, filename):
-    if not use_mlflow():
-        return
-
     if not _is_pyarrow_installed():
         logger.warning(f"Pyarrow is not installed. {filename} artifact won't be stored on HDFS")
         return

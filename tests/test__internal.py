faabab0541e1c76a7f0bf1adc2995df14cb838ae
Fabian Höring <f.horing@criteo.com>
2019-12-13 18:23:45 +0100

    Removed unused create_and_pack_conda_env
    Change-Id: Ibca2c35ed2c941e6e538a54c2e3c29cd8a22b7f4

--- tests/test__internal.py
+++ tests/test__internal.py
@@ -12,8 +12,7 @@ import pytest
 from tf_yarn._internal import (
     MonitoredThread,
     reserve_sock_addr,
-    xset_environ,
-    create_and_pack_conda_env,
+    xset_environ
 )
 
 
@@ -60,23 +59,3 @@ def conda_is_available():
         stderr=subprocess.PIPE,
         shell=True)
     return p.returncode == 0
-
-
-@pytest.mark.skipif(not conda_is_available(), reason="conda is not available")
-def test_create_conda_env(tmpdir):
-    env_zip_path = create_and_pack_conda_env(
-        name="test",
-        python="{0.major}.{0.minor}".format(sys.version_info),
-        pip_packages=["pycodestyle"],
-        root=str(tmpdir))
-    assert os.path.isfile(env_zip_path)
-    env_path, _zip = os.path.splitext(env_zip_path)
-    assert os.path.isdir(env_path)
-
-    env_unzipped_path = tmpdir.join("unzipped")
-    with zipfile.ZipFile(env_zip_path) as zf:
-        zf.extractall(env_unzipped_path)
-
-    env_python_bin = os.path.join(env_unzipped_path, "bin", "python")
-    os.chmod(env_python_bin, 0o755)
-    check_output([env_python_bin, "-m", "pycodestyle", "--version"])

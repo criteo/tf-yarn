d691c8b9bc4ba683fcd66f93b6fa55e22f69a7ab
Martin Bompaire <mbompr@gmail.com>
2021-03-04 10:51:55 +0100

    Fix mypy issue for target_data_fn

--- tf_yarn/keras_experiment.py
+++ tf_yarn/keras_experiment.py
@@ -1,11 +1,11 @@
 import tensorflow as tf
-from typing import NamedTuple, Callable
+from typing import NamedTuple, Callable, Optional
 
 
 class KerasExperiment(NamedTuple):
     model: tf.keras.models.Model
     model_dir: str
     train_params: dict
-    input_data_fn: Callable
-    target_data_fn: Callable
+    input_data_fn: Optional[Callable]
+    target_data_fn: Optional[Callable]
     validation_data_fn: Callable

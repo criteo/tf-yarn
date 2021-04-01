ee7d5194787a848b17d66e74a23a9b2b1e44db8e
Theophile Henri <t.henri@criteo.com>
2021-04-01 17:14:23 +0200

    Bump version to 0.6.2

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     include_package_data=True,
     package_data={"tf_yarn": ["default.log.conf"]},
-    version=_check_add_criteo_environment("0.6.1"),
+    version=_check_add_criteo_environment("0.6.2"),
     install_requires=REQUIREMENTS,
     tests_require=["pytest", "hadoop-test-cluster"],
     extras_require={

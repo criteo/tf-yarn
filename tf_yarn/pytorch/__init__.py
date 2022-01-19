f6bb75f3038c7b79eb1b56f86ef35239a9c9e36a
g.racic <g.racic@criteo.com>
2022-01-19 11:22:29 +0100

    Client for Pytorch training

--- tf_yarn/pytorch/__init__.py
+++ tf_yarn/pytorch/__init__.py
@@ -7,3 +7,18 @@ except ModuleNotFoundError:
            "You can install torch with 'pip install torch'"
            "or add it to the requirements.txt of your project.")
     warnings.warn(str)
+
+from tf_yarn.pytorch.client import run_on_yarn
+from tf_yarn.pytorch.experiment import PytorchExperiment
+from tf_yarn.client import get_safe_experiment_fn, RunFailed
+from tf_yarn.metrics import Metrics
+from tf_yarn.topologies import (
+    TaskSpec, NodeLabel
+)
+
+
+__all__ = [
+    "PytorchExperiment", "run_on_yarn", "RunFailed", "Metrics",
+    "TaskSpec", "NodeLabel", "get_safe_experiment_fn"
+]
+

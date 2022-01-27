8b518126dd0972c17e1ad26e7d01651d99d1c194
g.racic <g.racic@criteo.com>
2022-01-27 08:20:15 +0100

    pyenv_zip_path is now optional
    if None, the current python environment will be captured in a pex file.
    Also, editable requirements are captured and sent to Yarn executors for
    better user experience

--- tf_yarn/pytorch/client.py
+++ tf_yarn/pytorch/client.py
@@ -1,4 +1,4 @@
-from typing import Union, Dict, Callable, Optional
+from typing import Dict, Callable, Optional
 
 from tf_yarn import client
 from tf_yarn import topologies
@@ -10,7 +10,6 @@ ExperimentFn = Callable[[], PytorchExperiment]
 
 
 def run_on_yarn(
-    pyenv_zip_path: Union[str, Dict[topologies.NodeLabel, str]],
     experiment_fn: ExperimentFn,
     task_specs: Dict[str, topologies.TaskSpec],
     **kwargs
@@ -18,7 +17,6 @@ def run_on_yarn(
     if "custom_task_module" not in kwargs:
         kwargs["custom_task_module"] = "tf_yarn.pytorch.tasks.worker"
     return client.run_on_yarn(
-        pyenv_zip_path,
         experiment_fn,
         task_specs,
         **kwargs

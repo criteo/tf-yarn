5d0ab45fba232a1edee3501fb30ac639ef70a8f0
j.cuquemelle <j.cuquemelle@criteo.com>
2022-09-27 09:44:32 +0200

    Handle .pex.zip files built by cluster-pack
    Cluster-pack gives the possibility to handle pexes > 2GB but they need to
    be handled differently from standard pex (must be unzipped and the command
    is different)

--- tf_yarn/_env.py
+++ tf_yarn/_env.py
@@ -1,42 +1,10 @@
-import os
-from typing import (
-    Optional,
-    NamedTuple
-)
-
-import cluster_pack
-
+from typing import Optional
+from cluster_pack.packaging import PythonEnvDescription
 from tf_yarn import topologies
-
-
-class PythonEnvDescription(NamedTuple):
-    path_to_archive: str
-    dispatch_task_cmd: str
-    dest_path: str
 
 
 INDEPENDENT_WORKERS_MODULE = "tf_yarn.tensorflow.tasks._independent_workers_task"
 TENSORBOARD_MODULE = "tf_yarn.tensorflow.tasks._tensorboard_task"
-CONDA_ENV_NAME = "pyenv"
-CONDA_CMD = f"{CONDA_ENV_NAME}/bin/python"
-
-
-def gen_pyenv_from_existing_archive(path_to_archive: str) -> PythonEnvDescription:
-
-    archive_filename = os.path.basename(path_to_archive)
-
-    packer = cluster_pack.detect_packer_from_file(path_to_archive)
-    if packer == cluster_pack.PEX_PACKER:
-        return PythonEnvDescription(
-            path_to_archive,
-            f"./{archive_filename}",
-            archive_filename)
-    elif packer == cluster_pack.CONDA_PACKER:
-        return PythonEnvDescription(
-            path_to_archive,
-            f"{CONDA_CMD}", CONDA_ENV_NAME)
-    else:
-        raise ValueError("Archive format unsupported. Must be .pex or conda .zip")
 
 
 def gen_task_cmd(pyenv: PythonEnvDescription,

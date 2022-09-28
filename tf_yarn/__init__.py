530a57f6df13411a61a65d8fdb46141a3cb3629c
j.cuquemelle <j.cuquemelle@criteo.com>
2022-09-28 15:31:05 +0000

    move examples in module
    And make them importable, to allow quickly testing envs
    or pipelines for examples

--- tf_yarn/__init__.py
+++ tf_yarn/__init__.py
@@ -2,7 +2,6 @@ from tf_yarn.client import get_safe_experiment_fn, RunFailed
 from tf_yarn.metrics import Metrics
 from tf_yarn.topologies import TaskSpec, NodeLabel, single_server_topology, ps_strategy_topology
 
-
 __all__ = [
     "RunFailed", "Metrics", "TaskSpec", "NodeLabel", "single_server_topology",
     "ps_strategy_topology", "get_safe_experiment_fn"
